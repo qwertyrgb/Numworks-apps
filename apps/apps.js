0fa3433fd4df9e0dce28df96ac18e195ece74e00
qwertyrgb <68125220+qwertyrgb@users.noreply.github.com>
2022-07-15 23:59:57 +0200

    Update apps.js

--- apps/apps.js
+++ apps/apps.js
@@ -9,6 +9,8 @@ angular.module('nwas').service('apps', function() {
     { name: "HexEdit", description: {en: "Hexadecimal editor", fr: "Éditeur hexadécimal"} },
     { name: "BadApple", description: {en: "The Bad Apple", fr: "Démo Bad Apple"} },
     { name: "Rickroll", description: {en: "The Rickroll", fr: "Le Rickroll"} },
+    { name: "Example", description: {en: "Example", fr: "Exemple"} },
+    { name: "Example-Cpp", description: {en: "Example cpp", fr: "Exemple cpp"} },
     { name: "UnitCircle", description: {en: "Unit circle", fr: "Cercle trigonométrique"} },
     { name: "CHIP-8", description: {en: "CHIP-8 interpreter", fr: "Interpréteur CHIP-8"} },
   ];
