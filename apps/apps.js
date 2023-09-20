0678c043e5d19dd0130b64b7d9b5376203544cac
qwertyrgb <68125220+qwertyrgb@users.noreply.github.com>
2023-09-20 18:24:22 +0200

    Removed troll
    Removed troll on video's app.elf

--- apps/apps.js
+++ apps/apps.js
@@ -9,7 +9,6 @@ angular.module('nwas').service('apps', function() {
     { name: "Image", description: {en: "An image", fr: "Une image"} },
     { name: "Ball", description: {en: "Haha ballz", fr: "Un objet en forme sphérique qui rebondit sur les bords de votre calculatrice"} },
     { name: "Fractals", description: {en: "Mandelbrot fractal + Burning Ship", fr: "fractale de Mandelbrot + navire vla chaud"} },
-    { name: "Golly", description: {en: "Conway's game of life", fr: "Golly"} },
-    { name: "Video", description: {en: "Video rick", fr: "rick video"} }
+    { name: "Golly", description: {en: "Conway's game of life", fr: "Golly"} }
   ];
 });
